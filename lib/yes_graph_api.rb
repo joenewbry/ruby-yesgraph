7953282007afb47f4dbfb4eb820de6dce1d43cbf
Joe Newbry <joenewbry@gmail.com>
2015-10-07 15:49:42 -0700

    Make class instead of module and only store version in version class

--- lib/yes_graph_api.rb
+++ lib/yes_graph_api.rb
@@ -1,5 +1,73 @@
 require "yes_graph_api/version"
 
-module YesGraphAPI
-  puts "We have liftoff"
+class YesGraphAPI
+  include ::HTTParty
+  format :json
+
+  def initialize(secret_key)
+    @options = {
+      "Authorization" => "Bearer #{secret_key}",
+    "Content-Type" => "application/json",
+    "Accept" => 'application/json'
+               }
+  end
+
+  def test
+    self.class.get('https://api.yesgraph.com/v0/test', :headers => @options)
+  end
+
+  def post_address_book(user_id, source_name, source_email, entries, source_type="gmail")
+    self.class.post("https://api.yesgraph.com/v0/address-book",
+      {
+      body: {
+        user_id: user_id,
+        source: {
+            name: source_name,
+            email: source_email,
+            type: source_type
+        },
+        entries: entries
+      }.to_json,
+      :headers => @options
+      }
+    )
+  end
+
+  # Returns ranked list of suggestions
+  def get_address_book(user_id)
+    self.class.get("https://api.yesgraph.com/v0/address-book/#{user_id}",
+      {
+      :headers => @options
+      }
+    )
+  end
+
+  # should be called to list all user IDs that have submitted their address books
+  def get_address_books
+    self.class.get('https://api.yesgraph.com/v0/address-books', :headers => @options)
+  end
+
+  # should be called to create or update user
+  def post_users(users)
+    self.class.post('https://api.yesgraph.com/v0/users', {
+      :body => users.to_json,
+      :headers => @options,
+        })
+  end
+
+  def post_invite_sent(user_id, email)
+    self.class.post('https://api.yesgraph.com/v0/invite-sent',
+      {
+        :body => {user_id: user_id, email: email}.to_json,
+        :headers => @options
+      })
+  end
+
+  def post_invite_accepted(email)
+    self.class.post('https://api.yesgraph.com/v0/invite-accepted',
+      {
+        :body => {"email" => email}.to_json,
+        :headers => @options
+      })
+  end
 end
--- lib/yes_graph_api/version.rb
+++ lib/yes_graph_api/version.rb
@@ -1,3 +1,5 @@
+# YesGraph API wrapper
+
 module YesGraphAPI
   VERSION = "0.1.0"
 end
